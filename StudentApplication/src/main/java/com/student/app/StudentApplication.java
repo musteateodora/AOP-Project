1446c7e1a403e12eb2737bf5a124cdf6a18f553f
musteateodora <mustea.teodora@gmail.com>
2020-04-05 19:10:15 +0300

    removed unused app url

--- StudentApplication/src/main/java/com/student/app/StudentApplication.java
+++ StudentApplication/src/main/java/com/student/app/StudentApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentApplication {
-    public static final String APP_URL = "http://localhost:8090/";
 
     public static void main(String[] args) {
         SpringApplication.run(StudentApplication.class, args);
